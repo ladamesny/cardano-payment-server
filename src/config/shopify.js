6533533e215dc5172e6320fd17c42de8313f7273
ladamesny <larry.adames@iohk.io>
2024-12-02 17:43:32 -0500

    update version of shopify

--- src/config/shopify.js
+++ src/config/shopify.js
@@ -1,9 +1,22 @@
 const Shopify = require('shopify-api-node');
 
 const shopify = new Shopify({
-  shopName: process.env.SHOPIFY_SHOP_NAME,
+  shopName: process.env.SHOP_NAME,
   accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
-  apiVersion: '2023-10', // Using latest stable version
+  apiVersion: '2024-01', // Use the latest API version
 });
+
+// Add GraphQL support
+shopify.graphql = async function (query, variables = {}) {
+  const response = await this.request({
+    method: 'POST',
+    url: '/admin/api/2024-01/graphql.json',
+    data: {
+      query,
+      variables,
+    },
+  });
+  return response.data.data;
+};
 
 module.exports = shopify;
